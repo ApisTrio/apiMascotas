e3955be9f417a7cdc0362f76e6a28c6275c6be80
Daniel Torres <danieljtorres94@gmail.com>
2017-05-23 17:54:46 -0400

    Borrar archivos carpeta info

--- app/lib/database.php
+++ app/lib/database.php
@@ -7,7 +7,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=bodegadb;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=localhost;dbname=apimascotasdb;charset=utf8', 'root', '');
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
