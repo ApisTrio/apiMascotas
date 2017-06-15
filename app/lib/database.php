f01f9e451cadb30b6c897e3390ad984477bba494
Cis <kalinca15@gmail.com>
2017-06-15 09:42:38 -0400

    database hosting

--- app/lib/database.php
+++ app/lib/database.php
@@ -8,6 +8,9 @@ class Database
     public static function StartUp()
     {
         $pdo = new PDO('mysql:host=localhost;dbname=apimascotasdb;charset=utf8', 'root', '');
+
+        /*$pdo = new PDO('mysql:host=localhost;dbname=dinbeat7_apiMascota;charset=utf8', 'dinbeat7_api', '0.t0?q;xlq-}');*/
+
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
