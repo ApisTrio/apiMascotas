be0f70f8d747bd559bccced74ec90d09824a8c42
Daniel Torres <danieljtorres94@gmail.com>
2017-07-09 21:46:33 -0400

    Remover super busqueda

--- app/model/Admin.php
+++ app/model/Admin.php
@@ -177,69 +177,7 @@ class Admin
         }  
     }
 
-    public function superBusqueda($args)
-    {
-        try
-        {
-            $result = array();
 
-            if ( !empty($args[]) ) {
-                $sql = "SELECT usuarios.idusuario, usuarios.usuario, duenos.nombre, duenos.apellido, duenos.telefono, duenos.pais, duenos.provincia, duenos.ciudad, duenos.codigo_postal, usuarios.emailU, usuarios.duenos_idDueno FROM usuarios INNER JOIN duenos ON usuarios.duenos_idDueno = duenos.idDueno"
-            }
-
-
-            $query = $this->db->prepare("");
-            $query->execute();
-
-            $usuarios = $query->fetchAll();
-
-            foreach ($usuarios as $keyu => $usuario) {
-                
-                $query2 = $this->db->prepare("SELECT mascotas.idMascota, mascotas.nombre, DATE_FORMAT(mascotas.fecha_nacimiento,'%d/%m/%Y') as fecha_nacimiento, razas.raza, especies.especie 
-                    FROM mascotas 
-                    INNER JOIN razas ON mascotas.razas_idRaza = razas.idRaza 
-                    INNER JOIN especies ON razas.especies_idEspecie = especies.idEspecie 
-                    INNER JOIN duenos_has_mascotas ON duenos_has_mascotas.mascotas_idMascota  = mascotas.idMascota 
-                    INNER JOIN duenos ON duenos.idDueno = duenos_has_mascotas.duenos_idDueno
-                    WHERE duenos.idDueno = ?
-                    AND mascotas.borrado IS NULL
-                    GROUP BY idMascota");
-                $query2->execute([$usuario->duenos_idDueno]);
-
-                $mascotas = $query2->fetchAll();
-
-                foreach ($mascotas as $keym => $mascota) {
-
-                    $query3 = $this->db->prepare("SELECT placas.idPlaca, placas.codigo, modelos.modelo FROM placas 
-                        INNER JOIN mascotas_has_placas on placas.idPlaca = mascotas_has_placas.placas_idPlaca 
-                        INNER JOIN modelos on mascotas_has_placas.modelos_idModelo = modelos.idModelo
-                        WHERE mascotas_has_placas.mascotas_idMascota = ? AND 
-                        mascotas_has_placas.borrado IS NULL");
-
-                    $query3->execute([$mascota->idMascota]);
-
-                    $placas = $query3->fetchAll();
-
-                    $mascotas[$keym]->placas = $placas;                    
-               
-                }
-
-                $usuarios[$keyu]->mascotas = $mascotas;
-
-
-            }
-
-            $this->response->setResponse(true);
-            $this->response->result = $usuarios;
-            return $this->response;
-
-        }
-        catch(Exception $e)
-        {
-            $this->response->setResponse(false, $e->getMessage());
-            return $this->response;
-        }  
-    }
     
 
 }
