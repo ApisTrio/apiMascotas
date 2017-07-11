0fe1f6624d0db2574847541b1aa679d9cde2d7c6
Daniel Torres <danieljtorres94@gmail.com>
2017-07-11 01:30:33 -0400

    Arreglos en la funcion de superbusqueda

--- app/model/Admin.php
+++ app/model/Admin.php
@@ -124,7 +124,7 @@ class Admin
         {
             $result = array();
 
-            $query = $this->db->prepare("SELECT usuarios.idusuario, usuarios.usuario, duenos.nombre, duenos.apellido, duenos.telefono, duenos.pais, duenos.provincia, duenos.ciudad, duenos.codigo_postal, usuarios.emailU, usuarios.duenos_idDueno FROM usuarios INNER JOIN duenos ON usuarios.duenos_idDueno = duenos.idDueno");
+            $query = $this->db->prepare("SELECT usuarios.idusuario, usuarios.usuario, usuarios.activo, duenos.nombre, duenos.apellido, duenos.telefono, duenos.pais, duenos.provincia, duenos.ciudad, duenos.codigo_postal, usuarios.emailU, usuarios.duenos_idDueno FROM usuarios INNER JOIN duenos ON usuarios.duenos_idDueno = duenos.idDueno");
             $query->execute();
 
             $usuarios = $query->fetchAll();
@@ -146,7 +146,7 @@ class Admin
 
                 foreach ($mascotas as $keym => $mascota) {
 
-                    $query3 = $this->db->prepare("SELECT placas.idPlaca, placas.codigo, modelos.modelo FROM placas 
+                    $query3 = $this->db->prepare("SELECT placas.idPlaca, placas.codigo, modelos.modelo, modelos.forma FROM placas 
                         INNER JOIN mascotas_has_placas on placas.idPlaca = mascotas_has_placas.placas_idPlaca 
                         INNER JOIN modelos on mascotas_has_placas.modelos_idModelo = modelos.idModelo
                         WHERE mascotas_has_placas.mascotas_idMascota = ? AND 
@@ -184,33 +184,29 @@ class Admin
             $result = array();
 
             $nexo = "";
-            $sql_usuarios = "SELECT usuarios.idusuario, usuarios.usuario, duenos.nombre, duenos.apellido, duenos.telefono, duenos.pais, duenos.provincia, duenos.ciudad, duenos.codigo_postal, usuarios.emailU, usuarios.duenos_idDueno FROM usuarios INNER JOIN duenos ON usuarios.duenos_idDueno = duenos.idDueno";
+            $sql_usuarios = "SELECT usuarios.idusuario, usuarios.usuario, usuarios.activo, duenos.nombre, duenos.apellido, duenos.telefono, duenos.pais, duenos.provincia, duenos.ciudad, duenos.codigo_postal, usuarios.emailU, usuarios.duenos_idDueno FROM usuarios INNER JOIN duenos ON usuarios.duenos_idDueno = duenos.idDueno";
 
-            if ( !empty($args['usuario']) ) {
+            if ( isset($args['usuario']) or !empty($args['usuario']) ) {
                 if ($nexo == "") { $sql_usuarios .= " WHERE"; $nexo = " WHERE"; }
                 $sql_usuarios .= " usuarios.usuario LIKE '%".$args['usuario']."%'";
             }
-            if ( !empty($args['telefono']) ) {
+            if ( isset($args['telefono']) or !empty($args['telefono']) ) {
                 if ($nexo == "") { $sql_usuarios .= " WHERE"; } else if ($nexo == " WHERE") { $sql_usuarios .= " AND"; $nexo = " AND"; }
                 $sql_usuarios .= " duenos.telefono LIKE '%".$args['telefono']."%'";
             }
-            if ( !empty($args['email']) ) {
+            if ( isset($args['email']) or !empty($args['email']) ) {
                 if ($nexo == "") { $sql_usuarios .= " WHERE"; } else if ($nexo == " WHERE") { $sql_usuarios .= " AND"; $nexo = " AND"; }
                 $sql_usuarios .= " usuarios.emailU LIKE '%".$args['email']."%'";
             }
-            if ( !empty($args['email']) ) {
-                if ($nexo == "") { $sql_usuarios .= " WHERE"; } else if ($nexo == " WHERE") { $sql_usuarios .= " AND"; $nexo = " AND"; }
-                $sql_usuarios .= " usuarios.emailU LIKE '%".$args['email']."%'";
-            }
-            if ( !empty($args['pais']) ) {
+            if ( isset($args['pais']) or !empty($args['pais']) ) {
                 if ($nexo == "") { $sql_usuarios .= " WHERE"; } else if ($nexo == " WHERE") { $sql_usuarios .= " AND"; $nexo = " AND"; }
                 $sql_usuarios .= " duenos.pais LIKE '%".$args['pais']."%'";
             }
-            if ( !empty($args['ciudad_provincia']) ) {
+            if ( isset($args['ciudad_provincia']) or !empty($args['ciudad_provincia']) ) {
                 if ($nexo == "") { $sql_usuarios .= " WHERE"; } else if ($nexo == " WHERE") { $sql_usuarios .= " AND"; $nexo = " AND"; }
                 $sql_usuarios .= " duenos.provincia LIKE '%".$args['ciudad_provincia']."%' OR duenos.ciudad LIKE '%".$args['ciudad_provincia']."%'";
             }
-            if ( !empty($args['codigo_postal']) ) {
+            if ( isset($args['codigo_postal']) or !empty($args['codigo_postal']) ) {
                 if ($nexo == "") { $sql_usuarios .= " WHERE"; } else if ($nexo == " WHERE") { $sql_usuarios .= " AND"; $nexo = " AND"; }
                 $sql_usuarios .= " duenos.codigo_postal LIKE '%".$args['codigo_postal']."%'";
             }
@@ -227,25 +223,30 @@ class Admin
                     INNER JOIN razas ON mascotas.razas_idRaza = razas.idRaza 
                     INNER JOIN especies ON razas.especies_idEspecie = especies.idEspecie 
                     INNER JOIN duenos_has_mascotas ON duenos_has_mascotas.mascotas_idMascota  = mascotas.idMascota 
-                    INNER JOIN duenos ON duenos.idDueno = duenos_has_mascotas.duenos_idDueno
-                    INNER JOIN perdidas ON perdidas.mascotas_idMascota = idMascota
-                    WHERE duenos.idDueno = ?
-                    AND mascotas.borrado IS NULL";
+                    INNER JOIN duenos ON duenos.idDueno = duenos_has_mascotas.duenos_idDueno";
                 
-                if ( !empty($args['mascota']) ) {
+
+                if ( isset($args['perdida']) and $args['perdida'] == false or !empty($args['perdida']) and $args['perdida'] == false) {
+                    $sql_mascotas .= " INNER JOIN perdidas ON perdidas.mascotas_idMascota = idMascota WHERE duenos.idDueno = ? AND mascotas.borrado IS NULL AND perdidas.encontrado IS NOT NULL";
+                }else if ( isset($args['perdida']) and $args['perdida'] == true or !empty($args['perdida']) and $args['perdida'] == true) {
+                    $sql_mascotas .= " INNER JOIN perdidas ON perdidas.mascotas_idMascota = idMascota WHERE duenos.idDueno = ? AND mascotas.borrado IS NULL AND perdidas.encontrado IS NULL";
+                }
+                if ( !isset($args['perdida']) or empty($args['perdida']) ){
+                    $sql_mascotas .= " WHERE duenos.idDueno = ? AND mascotas.borrado IS NULL";
+                } 
+                if ( isset($args['mascota']) or !empty($args['mascota']) ) {
                     $sql_mascotas .= " AND mascotas.nombre LIKE '%".$args['mascota']."%'";
-                }   
-                if ( !empty($args['especie']) ) {
+                }  
+                if ( isset($args['fecha']) or !empty($args['fecha']) ) {
+                    $sql_mascotas .= " AND mascotas.fecha_nacimiento = STR_TO_DATE( '".$args['fecha']."', '%d/%m/%Y') ";
+                } 
+                if ( isset($args['especie']) or !empty($args['especie']) ) {
                     $sql_mascotas .= " AND especies.especie LIKE '%".$args['especie']."%'";
                 }                
-                if ( !empty($args['raza']) ) {
+                if ( isset($args['raza']) or !empty($args['raza']) ) {
                     $sql_mascotas .= " AND razas.raza LIKE '%".$args['raza']."%'";
                 }                  
-                if ( $args['perdida'] = "encontradas" ) {
-                    $sql_mascotas .= " AND perdidas.encontrado IS NOT NULL";
-                }else if ($args['perdida'] = "perdidas") {
-                    $sql_mascotas .= " AND perdidas.encontrado IS NULL";
-                }              
+            
                 $sql_mascotas .= " GROUP BY idMascota";
 
                 $query2 = $this->db->prepare($sql_mascotas);
@@ -255,16 +256,16 @@ class Admin
 
                 foreach ($mascotas as $keym => $mascota) {
 
-                    $sql_placas = "SELECT placas.idPlaca, placas.codigo, modelos.modelo FROM placas 
+                    $sql_placas = "SELECT placas.idPlaca, placas.codigo, modelos.modelo, modelos.forma FROM placas 
                         INNER JOIN mascotas_has_placas on placas.idPlaca = mascotas_has_placas.placas_idPlaca 
                         INNER JOIN modelos on mascotas_has_placas.modelos_idModelo = modelos.idModelo
                         WHERE mascotas_has_placas.mascotas_idMascota = ? AND 
                         mascotas_has_placas.borrado IS NULL";
 
-                    if ( !empty($args['id']) ) {
+                    if ( isset($args['id']) or !empty($args['id']) ) {
                         $sql_placas .= " AND placas.codigo LIKE '%".$args['id']."%'";
                     }                    
-                    if ( !empty($args['forma']) ) {
+                    if ( isset($args['forma']) or !empty($args['forma']) ) {
                         $sql_placas .= " AND modelos.forma = '".$args['forma']."'";
                     }  
 
@@ -277,20 +278,24 @@ class Admin
                     if ( count($placas) > 0 ) {
                         $mascotas[$keym]->placas = $placas; 
                     }else{
-                        unset($mascotas[$keym]);    
+                        unset($mascotas[$keym]);
+ 
                     }                   
                
                 }
+
+                $mascotas = array_values($mascotas); 
 
                 if ( count($mascotas) > 0 ) {
                     $usuarios[$keyu]->mascotas = $mascotas;
                 }else{
                     unset($usuarios[$keyu]);
+                    
                 }
 
-
-
             }
+
+            $usuarios = array_values($usuarios);
 
             $this->response->setResponse(true);
             $this->response->result = $usuarios;
