5d63fe61640ca579476d44b5df98c936efe4bef9
Cis <kalinca15@gmail.com>
2017-07-19 19:05:49 -0400

    excel placas

--- app/route/admins.php
+++ app/route/admins.php
@@ -273,4 +273,102 @@ $app->group('/admin/', function () {
             json_encode($Response = array('response' => "True" ) )
         );*/
     });
+
+	 $this->post('excel-placas', function ($req, $res, $args) {
+
+    	$datos = $req->getParsedBody()['datos'];
+
+        $titulo = 'Listado-de-placas';
+             /** Error reporting */
+            error_reporting(E_ALL);
+            ini_set('display_errors', TRUE);
+            ini_set('display_startup_errors', TRUE);
+            date_default_timezone_set('Europe/London');
+            if (PHP_SAPI == 'cli')
+              die('This example should only be run from a Web Browser');
+            /** Include PHPExcel */
+            require_once dirname(__FILE__) . '/../lib/PHPExcel.php';
+            // Create new PHPExcel object
+            $objPHPExcel = new PHPExcel();
+            // Set document properties
+            $objPHPExcel->getProperties()->setCreator("Dinbeat")
+                           ->setTitle($titulo)
+                           ->setDescription("Placas Generada");
+            // Add some data
+            $objPHPExcel->setActiveSheetIndex(0)
+                        ->setCellValue('A1', 'ID')
+                        ->setCellValue('B1', 'ACTIVA')
+                        ->setCellValue('C1', 'BORRADO')
+                        ->setCellValue('D1', 'FORMA')
+                        ->setCellValue('E1', 'NOMBRE MODELO')
+                        ->setCellValue('F1', 'MODELO');
+
+            // Miscellaneous glyphs, UTF-8
+            $y=2;
+
+            
+            foreach ($datos as $placa) {
+            	if (!$placa["bloqueado"] && !$placa["borrado"] ){
+            		$activa = "SI";
+            	}
+            	else{
+            		$activa = "NO";
+            	}
+
+            	if ($placa["borrado"] ){
+            		$borrado = "SI";
+            	}
+            	else{
+            		$borrado = "NO";
+            	}
+			    $objPHPExcel->setActiveSheetIndex(0)
+			    			->setCellValue('A'.$y, $placa["codigo"])
+				    		->setCellValue('B'.$y, $activa)
+				    		->setCellValue('C'.$y, $borrado)
+				    		->setCellValue('D'.$y, $placa["forma"])
+				    		->setCellValue('E'.$y, $placa["nombre"])
+				    		->setCellValue('F'.$y, 'dinbeat.com/qr/assets/images/placas/'.$placa["forma"].'/'.$placa["modelo"]);
+				    		$y++;
+
+            }
+
+            // Rename worksheet
+            $objPHPExcel->getActiveSheet()->setTitle($titulo);
+            // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+            $objPHPExcel->setActiveSheetIndex(0);
+            // Redirect output to a client’s web browser (Excel5)
+            header('Content-Type: application/vnd.ms-excel');
+            header('Content-Disposition: attachment;filename='.$titulo.'.xls');
+            header('Cache-Control: max-age=0');
+            // If you're serving to IE 9, then the following may be needed
+            header('Cache-Control: max-age=1');
+            // If you're serving to IE over SSL, then the following may be needed
+            header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+            header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+            header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+            header ('Pragma: public'); // HTTP/1.0
+            $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+            $objWriter->save('excel/'.$titulo.'.xls');
+
+
+            //$objWriter->save('php://output');
+           return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(array('response' => true, 'archivo' => $titulo.'.xls'))
+            );
+            
+            exit; 
+
+        //////////////////
+
+
+       /* return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode($Response = array('response' => "True" ) )
+        );*/
+    });
 });
